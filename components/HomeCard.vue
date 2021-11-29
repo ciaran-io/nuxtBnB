ec2612d6f8f585ac72278146f8f5a47993af52af
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2021-11-29 18:36:21 +0000

    Added base home card component.

---
+++ components/HomeCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  props: {
+    home: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<template>
+  <div>
+    <img
+      :src="home.images[0]"
+      style="object-fit: fill"
+      width="200"
+      height="200"
+      alt=""
+    />
+    <h2>{{ home.title }}</h2>
+    <p>{{ home.location.address }}</p>
+    <p>{{ home.location.city }}</p>
+    <p>{{ home.location.state }}</p>
+    <p>{{ home.guests }} guests</p>
+    <p>
+      <span>${{ home.pricePerNight }}</span>
+      / night
+    </p>
+  </div>
+</template>
