c5c78f5cab59f8cbdcae69d8961eb899f6ccd13b
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2021-11-29 18:37:26 +0000

    1. Added base setup for importing home data. 2. Addded base seo setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <Tutorial/>
+  <div class="grid grid-flow-col gap-1">
+    <HomeCard v-for="home in homes" :key="home.objectID" :home="home" />
+  </div>
 </template>
 
 <script>
-export default {}
+import homes from '~/data/homes'
+
+export default {
+  head: () => ({
+    title: 'Homepage',
+    meta: [
+      {
+        name: 'decription',
+        content: 'This is a homepage',
+        hid: 'decription',
+      },
+    ],
+  }),
+
+  data: () => ({
+    homes: homes.slice(0, 3),
+  }),
+}
 </script>
