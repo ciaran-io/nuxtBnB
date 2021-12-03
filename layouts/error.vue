7c163c3ca1465bbf84dd8c37fcdd6acd9a04f231
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2021-12-03 19:13:25 +0000

    Added basic error page

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  head() {
+    return {
+      title: `Error:(${this.error.statusCode})  `,
+    }
+  },
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+// Todo style error page and add image
+<template>
+  <div class="bg-red-400">
+    we have a problem {{ error.message }} {{ error.statusCode }}
+  </div>
+</template>
