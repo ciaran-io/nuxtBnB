efe2ee2f0365b23f74e7307e8317e1980fe89bb5
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2021-12-09 18:33:34 +0000

    1. Added mounted method to pull data from maps plugin.
    2. Added changed & router method to send input queries to sarch page.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,32 @@
+<script>
+export default {
+  mounted() {
+    this.$maps.makeAutoComplete(this.$refs.citySearch)
+  },
+  methods: {
+    changed(event) {
+      const place = event.detail
+      if (!place.geometry) return
+
+      this.$router.push({
+        name: 'search',
+        query: {
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng(),
+          label: this.$refs.citySearch.value,
+        },
+      })
+    },
+  },
+}
+</script>
+
 <template>
   <div>
     <header>
       <NuxtLink to="/">Home</NuxtLink>
+      <input ref="citySearch" type="text" @changed="changed" />
     </header>
     <nuxt />
   </div>
 </template>
-n
