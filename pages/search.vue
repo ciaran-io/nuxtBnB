7201a03bc07410b52b9dcb41fce0d5dbc220eca1
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2021-12-11 20:10:48 +0000

    Added methods to retrieve map markers for all located homes in search. Used google fitBounds method.

--- pages/search.vue
+++ pages/search.vue
@@ -9,8 +9,10 @@ export default {
     this.label = to.query.label
     this.lat = to.query.lat
     this.lng = to.query.lng
+    this.updateMap()
     next()
   },
+
   async asyncData({ query, $dataApi }) {
     const data = await $dataApi.getHomesByLocation(query.lat, query.lng)
 
@@ -21,15 +23,59 @@ export default {
       lng: query.lng,
     }
   },
+
+  head() {
+    return {
+      title: `Homes around ${this.label}`,
+    }
+  },
+
+  mounted() {
+    this.updateMap()
+  },
+
+  methods: {
+    updateMap() {
+      this.$maps.showMap(
+        this.$refs.map,
+        this.lat,
+        this.lng,
+        this.getHomeMarkers()
+      )
+    },
+
+    getHomeMarkers() {
+      return this.homes.map((home) => {
+        return { ...home._geoloc }
+      })
+    },
+  },
 }
 </script>
 
 <template>
-  <div>
-    {{ lat }} / {{ lng }}/ {{ label }}
+  <div
+    class="md:grid-cols-[2fr,1fr] gutter grid gap-12 mt-[var(--gutter-default-top)] content-wrapper"
+  >
+    <div>
+      <h1>Results found for {{ label }}</h1>
 
-    <div v-for="home in homes" :key="home.objectID">
-      {{ home.title }}
+      <div v-if="homes.length > 0" class="mt-16">
+        <NuxtLink
+          v-for="home in homes"
+          :key="home.objectID"
+          :to="`/home/${home.objectID}`"
+        >
+          <HomeCardSearchResult :home="home" />
+        </NuxtLink>
+      </div>
+
+      <div v-else>
+        <h1>Oops! looks like there no match for this.</h1>
+      </div>
     </div>
+
+    <!-- google maps -->
+    <div ref="map" class="rounded-md h-[500px] mt-20"></div>
   </div>
 </template>
