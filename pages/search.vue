8fb28c0418f76a84fc43af5b026a4630c8541666
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2021-12-11 13:25:06 +0000

    Changed watchQuery for beforeRouteUpdate method. Issues with watchQuery not uodating in dev env.

--- pages/search.vue
+++ pages/search.vue
@@ -1,7 +1,19 @@
 <script>
 export default {
+  async beforeRouteUpdate(to, from, next) {
+    const data = await this.$dataApi.getHomesByLocation(
+      to.query.lat,
+      to.query.lng
+    )
+    this.homes = data.json.hits
+    this.label = to.query.label
+    this.lat = to.query.lat
+    this.lng = to.query.lng
+    next()
+  },
   async asyncData({ query, $dataApi }) {
-    const data = await $dataApi.GetHomesByLocation(query.lat, query.lng)
+    const data = await $dataApi.getHomesByLocation(query.lat, query.lng)
+
     return {
       homes: data.json.hits,
       label: query.label,
@@ -9,8 +21,6 @@ export default {
       lng: query.lng,
     }
   },
-
-  watchQuery: ['lat'],
 }
 </script>
 
