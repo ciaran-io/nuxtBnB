5ceda7dd866b5cd7bb47911ebbaaf96a9056ed31
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2021-12-16 18:43:11 +0000

    1. Moved unwrap & gettErrorResponse function to utils folder. 2. Changed headers to use publicRuntime config in nuxt config.

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,105 +1,90 @@
-export default function (context, inject) {
-  const appId = '20TT3H3J0T'
-  const apiKey = 'a2ab745a479ac514dd5769f72997579d'
-  const headers = {
-    'X-Algolia-API-Key': apiKey,
-    'X-Algolia-Application-Id': appId,
-  }
+import {unWrap, getErrorResponse} from '~/utils/fetchUtils'
 
-  inject('dataApi', {
-    getHome,
-    getReviewsByHomeId,
-    getUserByHomeId,
-    getHomesByLocation,
-  })
-
-  async function getHome(homeId) {
-    try {
-      return unWrap(
-        await fetch(
-          `https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
-          { headers }
+  export default function ({ $config }, inject) {
+    
+    const headers = {
+      'X-Algolia-API-Key': $config.algolia.key,
+      'X-Algolia-Application-Id': $config.algolia.appId
+      }
+  
+    inject('dataApi', {
+      getHome,
+      getReviewsByHomeId,
+      getUserByHomeId,
+      getHomesByLocation,
+    })
+  
+    async function getHome(homeId) {
+      try {
+        return unWrap(
+          await fetch(
+            `https://${$config.algolia.appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
+            { headers }
+          )
         )
-      )
-    } catch (error) {
-      return getErrorResponse(error)
+      } catch (error) {
+        return getErrorResponse(error)
+      }
     }
-  }
-
-  async function getReviewsByHomeId(homeId) {
-    try {
-      return unWrap(
-        await fetch(
-          `https://${appId}-dsn.algolia.net/1/indexes/reviews/query`,
-          {
+  
+    async function getReviewsByHomeId(homeId) {
+      try {
+        return unWrap(
+          await fetch(
+            `https://${$config.algolia.appId}-dsn.algolia.net/1/indexes/reviews/query`,
+            {
+              headers,
+              method: 'POST',
+              body: JSON.stringify({
+                filters: `homeId:${homeId}`,
+                hitsPerPage: 6,
+                attributesToHighlight: [],
+              }),
+            }
+          )
+        )
+      } catch (error) {
+        return getErrorResponse(error)
+      }
+    }
+  
+    async function getUserByHomeId(homeId) {
+      try {
+        return unWrap(
+          await fetch(`https://${$config.algolia.appId}-dsn.algolia.net/1/indexes/users/query`, {
             headers,
             method: 'POST',
             body: JSON.stringify({
               filters: `homeId:${homeId}`,
-              hitsPerPage: 6,
               attributesToHighlight: [],
             }),
-          }
+          })
         )
-      )
-    } catch (error) {
-      return getErrorResponse(error)
+      } catch (error) {
+        return getErrorResponse(error)
+      }
     }
+  
+    async function getHomesByLocation(Lat, Lng, radiusInMeters = 1500) {
+      try {
+        return unWrap(
+          await fetch(`https://${$config.algolia.appId}-dsn.algolia.net/1/indexes/homes/query`, {
+            headers,
+            method: 'POST',
+            body: JSON.stringify({
+              aroundRadius: radiusInMeters,
+              aroundLatLng: `${Lat}, ${Lng}`,
+              hitsPerPage: 10,
+              attributesToHighlight: [],
+            }),
+          })
+        )
+      } catch (error) {
+        return getErrorResponse(error)
+      }
+    }
+  
+    
   }
 
-  async function getUserByHomeId(homeId) {
-    try {
-      return unWrap(
-        await fetch(`https://${appId}-dsn.algolia.net/1/indexes/users/query`, {
-          headers,
-          method: 'POST',
-          body: JSON.stringify({
-            filters: `homeId:${homeId}`,
-            attributesToHighlight: [],
-          }),
-        })
-      )
-    } catch (error) {
-      return getErrorResponse(error)
-    }
-  }
 
-  async function getHomesByLocation(Lat, Lng, radiusInMeters = 1500) {
-    try {
-      return unWrap(
-        await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/query`, {
-          headers,
-          method: 'POST',
-          body: JSON.stringify({
-            aroundRadius: radiusInMeters,
-            aroundLatLng: `${Lat}, ${Lng}`,
-            hitsPerPage: 10,
-            attributesToHighlight: [],
-          }),
-        })
-      )
-    } catch (error) {
-      return getErrorResponse(error)
-    }
-  }
-
-  async function unWrap(response) {
-    const json = await response.json()
-    const { ok, status, statusText } = response
-    return {
-      json,
-      ok,
-      status,
-      statusText,
-    }
-  }
-
-  function getErrorResponse(error) {
-    return {
-      ok: !true,
-      status: 500,
-      statusText: error.message,
-      json: {},
-    }
-  }
-}
