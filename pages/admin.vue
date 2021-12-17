f53d31c6db062432f4b42ff5d3fdd8eed6ee9d62
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2021-12-17 12:32:58 +0000

    Added base admin pages and deny access to admin page route.

---
+++ pages/admin.vue
@@ -0,0 +1,23 @@
+<script>
+import Cookie from 'js-cookie'
+export default {
+asyncData({$config, redirect}){
+  if(!Cookie.get($config.auth.cookieName)){
+    redirect('/deny-access/')
+    return
+  }
+}
+}
+
+</script>
+
+export default {
+
+}
+
+<template>
+  <div>
+    admin
+    <NuxtChild/>
+  </div>
+</template>
