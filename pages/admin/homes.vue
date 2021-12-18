5f688c540495a908f95de4f52f642de7700ac39a
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2021-12-18 16:24:21 +0000

    Added google places functionality to update address fields.

--- pages/admin/homes.vue
+++ pages/admin/homes.vue
@@ -1,6 +1,5 @@
 <script>
 
-
 export default {
     data: () => ({
         home: {
@@ -18,17 +17,44 @@ export default {
                 city: "",
                 state: "",
                 postalCode: "",
-                counrty: "",
+                country: "",
             },
             __geoloc: {
-                lat: 26.1,
-                lng: 26.1
+                lat: '',
+                lng: ''
             },
             images: []
         }
     }),
 
+  mounted (){
+  // this.$maps.makeAutoComplete(this.$refs.locationSelector, ['address'])
+    this.$maps.makeAutoComplete(this.$refs.locationSelector, ['address'])
+  },
+
 methods: {
+
+  changed(event){
+    const addressParts = event.detail.address_components
+    const street = this.getAddressPart(addressParts, 'street_number')?.short_name || ''
+    const route = this.getAddressPart(addressParts, 'route')?.short_name || ''
+    
+    this.home.location.address = street + ' ' + route
+    this.home.location.city = this.getAddressPart(addressParts, 'locality')?.short_name || ''
+    this.home.location.state = this.getAddressPart(addressParts, 'administrative_area_level_1')?.long_name || ''
+    this.home.location.country = this.getAddressPart(addressParts, 'country')?.short_name || ''
+    this.home.location.postalCode = this.getAddressPart(addressParts, 'postal_code')?.short_name || ''
+
+
+    const geo = event.detail.geometry.location
+    this.home.__geoloc.lat = geo.lat()
+    this.home.__geoloc.lng = geo.lng()
+  },
+
+  getAddressPart(parts, type){
+    return parts.find(part => part.types.includes(type))
+  },
+
   async onSubmit(){
     await fetch('/api/homes', {
       method: 'POST',
@@ -39,7 +65,8 @@ methods: {
       }
     })
   }
-}
+},
+
 }
 </script>
 
@@ -65,23 +92,24 @@ methods: {
     <input  v-model="home.features[0]" type="text">
 
     Price Per Night
-    <input type="number" v-model="home.pricePerNight">
+    <input v-model="home.pricePerNight" type="number" >
 
     Guests / Rooms / Beds / bathrooms
 
-    <input type="number" v-model="home.guests">
-    <input type="number" v-model="home.bedrooms">
-    <input type="number" v-model="home.beds">
-    <input type="number" v-model="home.bathrooms">
+    <input v-model="home.guests" type="number">
+    <input v-model="home.bedrooms" type="number">
+    <input v-model="home.beds" type="number">
+    <input v-model="home.bathrooms" type="number">
 
-    Address: <input  v-model="home.location.address" type="text">
-    City: <input  v-model="home.location.city" type="text">
-    State: <input  v-model="home.location.state" type="text">
-    Postal Code: <input  v-model="home.location.PostalCode" type="text">
-    Counrty: <input  v-model="home.location.counrty" type="text">
+    <!-- <input type="text" ref="locationSelector" autocomplete="off"  placeholder="Select a location" @changed="changed"> -->
+    <input type="text" ref="locationSelector" autocomplete="off"  placeholder="Select a location" @changed="changed">
+    Address: <input v-model="home.location.address" type="text">
+    City: <input v-model="home.location.city" type="text">
+    State: <input v-model="home.location.state" type="text">
+    Postal Code: <input v-model="home.location.postalCode" type="text">
+    Counrty: <input v-model="home.location.country" type="text">
 
     <SubmitButton>Add</SubmitButton>
-
 
   </form>
 </div>
