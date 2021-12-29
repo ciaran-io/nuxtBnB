b8bcc071384e84c7920f161fe069f69147aeca4f
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2021-12-29 15:58:34 +0000

    refactor: update format & getSchema.

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,32 +1,6 @@
 <script>
 export default {
-  computed: {
-    // schema for enhancing position in search engines
-    getSchema() {
-      return JSON.stringify({
-        '@context': 'http://schema.org',
-        '@type': 'BedAndBreakfast',
-        name: this.home.title,
-        image: this.$img(
-          this.home.images[0],
-          { width: 1200 },
-          { provider: 'cloudinary' }
-        ),
-        address: {
-          '@type': PostalAddress,
-          addressLocality: this.home.location.city,
-          addressRegion: this.home.location.state,
-          postalCode: this.home.location.zipcode,
-          streetAddress: this.home.location.address,
-        },
-        aggregateRating: {
-          '@type': 'AggregateRating',
-          ratingValue: this.home.reviewValue,
-          reviewCount: this.home.reviewCount,
-        },
-      })
-    },
-  },
+  name: 'dynamicHomesResult',
   // call algolia api useing dataApi plugin
   async asyncData({ params, $dataApi, error }) {
     const responses = await Promise.all([
@@ -49,41 +23,73 @@ export default {
     }
   },
 
-  head() {
-    return {
-      title: this.home.title,
-      meta: [
-        // social media meta data for sharing
-        { hid: 'og-type', property: 'og:type', content: 'website' },
-        { hid: 'og-title', property: 'og:title', content: this.home.title },
-        {
-          hid: 'og-desc',
-          property: 'og:description',
-          content: this.home.description,
+  computed: {
+    // schema for enhancing position in search engines
+    getSchema() {
+      return JSON.stringify({
+        '@context': 'http://schema.org',
+        '@type': 'BedAndBreakfast',
+        name: this.home.title,
+        image: this.$img(
+          this.home.images[0],
+          { width: 1200 },
+          { provider: 'cloudinary' }
+        ),
+        address: {
+          '@type': 'PostalAddress',
+          addressLocality: this.home.location.city,
+          addressRegion: this.home.location.state,
+          postalCode: this.home.location.zipcode,
+          streetAddress: this.home.location.address,
         },
-        {
-          hid: 'og-image',
-          property: 'og:image',
-          content: this.$img(
-            this.home.images[0],
-            { width: 1200 },
-            { provider: 'cloudinary' }
-          ),
+        aggregateRating: {
+          '@type': 'AggregateRating',
+          ratingValue: this.home.reviewValue,
+          reviewCount: this.home.reviewCount,
         },
-        {
-          hid: 'og-url',
-          property: 'og:url',
-          content: `${this.$config.rootUrl}/home/${this.home.objectID}`,
-        },
-        { hid: 't-type', name: 'twitter:card', content: 'summary_large_image' },
+      })
+    },
 
-        {
-          name: 'description',
-          content: `This is a new home ${this.home.title}`,
-          hid: 'description',
-        },
-      ],
-    }
+    head() {
+      return {
+        title: this.home.title,
+        meta: [
+          // social media meta data for sharing
+          { hid: 'og-type', property: 'og:type', content: 'website' },
+          { hid: 'og-title', property: 'og:title', content: this.home.title },
+          {
+            hid: 'og-desc',
+            property: 'og:description',
+            content: this.home.description,
+          },
+          {
+            hid: 'og-image',
+            property: 'og:image',
+            content: this.$img(
+              this.home.images[0],
+              { width: 1200 },
+              { provider: 'cloudinary' }
+            ),
+          },
+          {
+            hid: 'og-url',
+            property: 'og:url',
+            content: `${this.$config.rootUrl}/home/${this.home.objectID}`,
+          },
+          {
+            hid: 't-type',
+            name: 'twitter:card',
+            content: 'summary_large_image',
+          },
+
+          {
+            name: 'description',
+            content: `This is a new home ${this.home.title}`,
+            hid: 'description',
+          },
+        ],
+      }
+    },
   },
 }
 </script>
